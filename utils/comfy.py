824cad4157c9cec684993de59f9727e2ba8c548a
YooSungHyun <shyu0522@hanmail.net>
2022-11-14 16:59:25 +0900

    Update: 배치 사이즈 선택을 위한 파라미터 추가

--- utils/comfy.py
+++ utils/comfy.py
@@ -1,9 +1,21 @@
 from pathlib import Path
 import json
 import os
+import argparse
 
 
 def read_json(fname):
     fname = Path(fname)
     with fname.open("rt") as handle:
         return json.load(handle)
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
