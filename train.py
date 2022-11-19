ac888f498cef84341e617036942f093660708eee
bart <shyu0522@hanmail.com>
2022-11-20 00:49:01 +0900

    Update: Train 최종

--- train.py
+++ train.py
@@ -1,6 +1,4 @@
-import torch
 import argparse
-import random
 from utils import read_json, str2bool
 import pytorch_lightning as pl
 from model import RNNTransducer
@@ -9,9 +7,12 @@ from pytorch_lightning.strategies import DDPStrategy
 from datetime import timedelta
 from setproctitle import setproctitle
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import LearningRateMonitor
 
 
 def main(hparams):
+    wandb_logger = WandbLogger(project="RNNTransducer", name="default", save_dir="./")
     setproctitle("bart_online_stt")
     pl.seed_everything(hparams.seed)
 
@@ -19,8 +20,6 @@ def main(hparams):
     # torch.distributed.init_process_group의 timeout은 30분으로, 대기를 30분 이상하면 자동으로 종료된다.
     # 때문에 대용량 Data는 절대로 완성되지 않을 경우가 존재하는데, 따라서 임시적으로 해당 파라미터만 조절한다.
     # Trainer의 초기화가 from_argparse_args에서 hparams의 dict key, value 쌍으로 이루어지므로, 이렇게 구동하여도 문제는 없다. (개발자 가이드에도 제안되는 방법임)
-    hparams.strategy = DDPStrategy(timeout=timedelta(days=30))
-
     model_config_dict = read_json(hparams.model_config)["model"]
     data_config_dict = read_json(hparams.model_config)["data"]
 
@@ -31,6 +30,10 @@ def main(hparams):
     model = RNNTransducer(
         model_config_dict["prednet"], model_config_dict["transnet"], model_config_dict["jointnet"], hparams
     )
+
+    wandb_logger.watch(model, log="all")
+    hparams.logger = wandb_logger
+
     checkpoint_callback = ModelCheckpoint(
         dirpath=hparams.output_dir,
         save_top_k=3,
@@ -38,7 +41,10 @@ def main(hparams):
         monitor="val_wer",
         filename="bart-online-{epoch:02d}-{val_wer:.4f}",
     )
-    hparams.callbacks = [checkpoint_callback]
+    lr_monitor = LearningRateMonitor(logging_interval="step")
+    hparams.callbacks = [checkpoint_callback, lr_monitor]
+
+    hparams.strategy = DDPStrategy(timeout=timedelta(days=30))
     trainer = pl.Trainer.from_argparse_args(hparams)
     trainer.fit(model, datamodule=RNNT_datamodule)
     checkpoint_callback.best_model_path
